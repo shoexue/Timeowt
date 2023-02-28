dd66329708f49e00453880a4a1a3e9fcd8759a80
Annika <annika.xu19@gmail.com>
2023-02-28 14:02:41 -0500

    Added switch and headings to popup

--- popup.js
+++ popup.js
@@ -1,13 +1,24 @@
-document.addEventListener("DOMContentLoaded", function(){
-    var checkPageButton = document.getElementById('clickIt');
-    checkPageButton.addEventListener('click', function(){
+document.addEventListener("DOMContentLoaded", 
+    function(){
+    var checkToggleswitch = document.getElementById('extonoff')
 
-        chrome.tabs.query({
-            active: true,
-            lastFocusedWindow: true
-        },function(tab){
-            alert("Timeowout!");
-        
-        });
-    }, false);
+    checkToggleswitch.addEventListener('change',function(){
+        if (checkToggleswitch.checked){
+            alert('hi');
+        }else{
+            alert('bye');
+        }
+    });
 }, false);
+
+    // var checkPageButton = document.getElementById('clickIt');
+    // checkPageButton.addEventListener('click', function(){
+
+    //     chrome.tabs.query({
+    //         active: true,
+    //         lastFocusedWindow: true
+    //     },function(tab){
+    //         alert("Timeowout!");
+        
+    //     });
+    // }, false);
