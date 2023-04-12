a1dd191f7a396b8ac40d9dfe2ddae5f61fcf9c4b
Annika <annika.xu19@gmail.com>
2023-04-12 12:40:15 -0400

    Updated popup.js so HTML reflects ext state

--- popup.js
+++ popup.js
@@ -1,24 +1,17 @@
-// chrome.runtime.getBackgroundPage(function(backgroundPage) {
-//     console = backgroundPage.console;
-//   })
-
-// function activateBackground() {
-//     chrome.runtime.sendMessage({action: "activate"});
-//   }
-
-// document.getElementById("checkbox").addEventListener("change", function() {
-//     if (this.checked) {
-//       chrome.runtime.sendMessage({activateBackgroundJS: true});
-//     } else {
-//       chrome.runtime.sendMessage({activateBackgroundJS: false});
-//     }
-//   });
-
 //Event DOMContentLoaded fires when initial HTML document has been completely loaded and parsed
 document.addEventListener("DOMContentLoaded", 
     function(){
     var checkToggleswitch = document.getElementById('extonoff')
 
+    //check local storage to see if extension was last turned on or off
+    chrome.storage.local.get(["extOn"], (result) => {
+        if (result.extOn==true){
+            //if extension is on, show toggle switch as "on" (checked) in popup
+            document.getElementById("extonoff").checked = true;
+        }
+    });
+
+    //check for change in toggle switch state, if on activate background script, if not don't
     checkToggleswitch.addEventListener('change',function(){
         if (checkToggleswitch.checked){
             chrome.runtime.sendMessage({activateBackgroundJS: true});
@@ -26,37 +19,15 @@ document.addEventListener("DOMContentLoaded",
             chrome.runtime.sendMessage({activateBackgroundJS: false});
         }
     });
-
-    
-    // if (document.getElementById('instagram').checked == true) {
-    //     console.log("instagram check");
-    // }
-    // var id = ['youtube','instagram','tiktok','other'];
-    // var checkTiktok = document.getElementById('tiktok');
-    
-    // checkTiktok.addEventListener('change',function(){
-    //     if (checkTiktok.checked){
-    //         console.log("tiktok");
-    //     }
-    // });
     
 });
 
 
+//ignore
 function checkClickFunc(){
     var checkbox = document.getElementById('instagram');
     if (checkbox.checked == true){
         alert("Checkbox is clicked");
     }
 }
-    // var checkPageButton = document.getElementById('clickIt');
-    // checkPageButton.addEventListener('click', function(){
-
-    //     chrome.tabs.query({
-    //         active: true,
-    //         lastFocusedWindow: true
-    //     },function(tab){
-    //         alert("Timeowout!");
-        
-    //     });
-    // }, false);
+  
